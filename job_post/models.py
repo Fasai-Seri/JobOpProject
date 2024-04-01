84c5d02abc6b0eabb6eb678a8dc0090663743464
Fasai Serikijcharoen <fasai.seri@gmail.com>
2024-04-01 19:55:12 +0700

    create jobpost model

--- job_post/models.py
+++ job_post/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from user_profiles.models import Employee, Professor
 # Create your models here.
+
+class JobPost(models.Model):
+    emp_id = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='job_posted_emp', blank=True)
+    prof_id = models.ForeignKey(Professor, on_delete=models.CASCADE, related_name='job_posted_prof', blank=True)
+    post_title = models.CharField(max_length=100)
+    post_desc = models.CharField(max_length=1000)
+    post_posted_date = models.DateField()
+    post_status = models.BooleanField()
