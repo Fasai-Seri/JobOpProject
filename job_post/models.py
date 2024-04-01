3d2ac022a06ac3348c49162a6981b8469ed31404
Fasai Serikijcharoen <fasai.seri@gmail.com>
2024-04-01 19:58:13 +0700

    fix jobpost model

--- job_post/models.py
+++ job_post/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from user_profiles.models import Employee, Professor
+from user_profiles.models import Employer, Professor
 # Create your models here.
 
 class JobPost(models.Model):
-    emp_id = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='job_posted_emp', blank=True)
+    emp_id = models.ForeignKey(Employer, on_delete=models.CASCADE, related_name='job_posted_emp', blank=True)
     prof_id = models.ForeignKey(Professor, on_delete=models.CASCADE, related_name='job_posted_prof', blank=True)
     post_title = models.CharField(max_length=100)
     post_desc = models.CharField(max_length=1000)
