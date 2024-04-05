b4c81b3b10ab47e8b5746ce57cf07a7a32934e8c
nipasri <141020992+nipasri@users.noreply.github.com>
2024-04-05 19:32:23 +0700

    try react user profile

---
+++ user_profiles/static/user_profiles/Components/ProfilePanel.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+export default ProfilePanel = (props) => {
+  return <div>This is a component</div>;
+};
---
+++ user_profiles/static/user_profiles/Javascript/profile_panel.js
@@ -0,0 +1,5 @@
+const ProfilePanel = (props) => {
+  return <div>This is a component</div>;
+};
+
+ReactDOM.render(<ProfilePanel />, document.querySelector("#profile"));
