86d9904ecb51b77e1f1a086ead65b75c7c25fe37
Fasai Serikijcharoen <fasai.seri@gmail.com>
2024-04-05 16:57:21 +0700

    improve model JobPost

--- job_post/models.py
+++ job_post/models.py
@@ -1,24 +1,33 @@
 from django.db import models
+from django.utils.timezone import now
+
 from user_profiles.models import *
 # Create your models here.
 
 class JobPost(models.Model):
-    job_type_choices = {
-        "Intern": "internship",
-        "Part Time": "part time",
-        "Full Time": "full time"
-    }
-    poster_emp_id = models.ForeignKey(Employer, on_delete=models.CASCADE, related_name='job_posted_emp', blank=True)
-    poster_prof_id = models.ForeignKey(Professor, on_delete=models.CASCADE, related_name='job_posted_prof', blank=True)
+    
+    job_type_choices = (
+        ("internship", "Intern"),
+        ("part time", "Part Time"),
+        ("full time", "full Time")
+    )
+    
+    job_status_choices = (
+        ("active", "Active"),
+        ("inactive", "Inactive")
+    )
+    
+    poster_emp = models.ForeignKey(Employer, on_delete=models.CASCADE, related_name='job_posted_emp', null=True)
+    poster_prof = models.ForeignKey(Professor, on_delete=models.CASCADE, related_name='job_posted_prof', null=True)
     job_title = models.CharField(max_length=100)
-    job_type = models.CharField(choices=job_type_choices)
-    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="job_posts_by_company")
-    job_desc = models.CharField(max_length=1000, blank=True)
-    job_requirements = models.CharField(max_length=1000, blank=True)
-    job_major = models.ManyToManyField(Major, related_name="job_posts_by_major", blank=True)
-    job_posted_date = models.DateField()
-    job_close_date = models.DateField(blank=True)
-    job_location = models.CharField(max_length=500)
-    job_status = models.BooleanField()
-    favourite_user = models.ManyToManyField(User, related_name="fovourit_posts", blank=True)
+    job_type = models.CharField(choices=job_type_choices, max_length=10, default='')
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="job_posts_by_company", default='')
+    job_desc = models.CharField(max_length=1000, null=True)
+    job_requirements = models.CharField(max_length=1000, null=True)
+    job_major = models.ManyToManyField(Major, related_name="job_posts_by_major")
+    job_posted_date = models.DateTimeField(default=now)
+    job_close_date = models.DateTimeField(null=True)
+    job_location = models.CharField(max_length=500, null=True)
+    job_status = models.CharField(choices=job_status_choices, max_length=10, default='')
+    favourite_user = models.ManyToManyField(User, related_name="fovourite_posts")
     
