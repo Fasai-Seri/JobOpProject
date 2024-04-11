fd978f962a45c8781f07752dc2823ca4ef989959
nipasri <141020992+nipasri@users.noreply.github.com>
2024-04-11 19:19:45 +0700

    create company app

--- job_opportunity/settings.py
+++ job_opportunity/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "job_post",
     "user_profiles",
+    'company',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
